844647d024fa9837a1c9ea9e7633e0d764db1be2
BiLaL <97440505+E9N99@users.noreply.github.com>
2025-08-20 13:16:54 +0300

    Update جات.py

--- zelz/assistant/جات.py
+++ zelz/assistant/جات.py
@@ -1,4 +1,34 @@
+import re
+import random
 import asyncio
+from collections import defaultdict
+from datetime import datetime
+from typing import Optional, Union
+
+from telethon import Button, events
+from telethon.errors import UserIsBlockedError
+from telethon.events import CallbackQuery, StopPropagation
+from telethon.utils import get_display_name
+
+from . import Config, zedub
+
+from ..core import check_owner, pool
+from ..core.logger import logging
+from ..core.session import tgbot
+from ..helpers import reply_id
+from ..helpers.utils import _format
+from ..sql_helper.bot_blacklists import check_is_black_list
+from ..sql_helper.bot_pms_sql import (
+    add_user_to_db,
+    get_user_id,
+    get_user_logging,
+    get_user_reply,
+)
+from ..sql_helper.bot_starters import add_starter_to_db, get_starter_details
+from ..sql_helper.globals import delgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID
+from .botmanagers import ban_user_from_bot
+
 from telethon import events, Button
 from telethon.tl.functions.messages import GetHistoryRequest
 
