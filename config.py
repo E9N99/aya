b2fb37598fdb79a36298ae14ea576c0a0f0d6eb0
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2025-09-23 23:59:05 +0300

    Update and rename exampleconfig.py to config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+rom sample_config import Config
+class Development(Config):
+    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي >
+    APP_ID  = 9671629
+    API_HASH = "be5c84e9dc1ca0e2b53d54b71e575124"
+    ALIVE_NAME = "اسم حسابك التلي"
+    DB_URI = "postgresql://postgres:shaham82@localhost:5432/shaham8"
+    STRING_SESSIO = "1ApWapzMBuwU7CDVia7t7lRYCCG6NrzIB0NSSvQNlzA8b2xbBHlz5plhTkh0qozgJekFb2CMQEhhzQIgeKn0Zn32Rr6007v-vgsApYK-JczVcKHY62pqFxafVKyAhJOY3_4oQg8-9ELIArvrW_WSoWyFgFmvniR1223-q6ipU_GUEQTtiKb4YHXvfXeHAe34IsdEKzAkytz61VW78l1KlxHE83yQSTH5Y_xnTstzyeDXGEfjd-_LutrsFhcZdNtVCDuzOWCSqlydWkuvOffu7LWL0Y3D49e1gj_xPx8p7Gl08iumJZ-Q-tXEWMMDCgfArBdgPRB9JYZgzjwTBIHNt9M-0p0-m-dw="
+    TG_BOT_TOKEN = "8192202802:AAGXwMFwGGhlALcb3b5BZzaDU7cS-12o058"
+    COMMAND_HAND_LER = "."  # اتركهـ
+    SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
+    TZ = "Asia/Baghdad"  # اتركهــا كمـ
